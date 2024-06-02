474fc1584d73e39157db2d1c478b5bbf2c621118
Willem Franco <willemfranco@gmail.com>
2024-06-02 17:35:17 -0500

    Composer update. - Adding Str `afterNext` to fix an unexpected behavior in the `after` method.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        // The `after` method is not working as expected, `afterNext` is a workaround.
+        Stringable::macro('afterNext', function (string $search) {
+            /** @var Stringable $this */
+            return $search === '' ? $this : new Stringable(
+                str_contains($this->value, $search) ? Str::after($this->value, $search) : ''
+            );
+        });
     }
 }
