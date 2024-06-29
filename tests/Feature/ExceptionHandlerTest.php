29cec3923859e6d2c89b733042339124b4530726
Willem Franco <willemfranco@gmail.com>
2024-06-28 22:20:59 -0500

    87. Error de Laravel por defecto fuera del prefijo 'api'

--- tests/Feature/ExceptionHandlerTest.php
+++ tests/Feature/ExceptionHandlerTest.php
@@ -9,14 +9,30 @@ class ExceptionHandlerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_json_api_route_error_shown_when_requesting_an_invalid_route()
+    public function test_format_route_error_only_on_api_routes_and_json_api_requests()
     {
-        $this->getJsonApi('api/test')
+        // A `JsonApi` call to `api` routes should return a JSON:API error response.
+        $this->getJsonApi('api/route')
             ->assertStatus(404)
             ->assertJsonApiError(
                 title: 'Not Found',
                 status: '404',
-                detail: 'The route api/test could not be found.',
+                detail: 'The route api/route could not be found.',
             );
+
+        // A `Json` call to `api` routes should return a default laravel json response.
+        $this->getJson('api/route')
+            ->assertStatus(404)
+            ->assertJson(['message' => 'The route api/route could not be found.']);
+
+        // A normal call to `api` routes should return a default laravel html response.
+        $this->get('api/route')
+            ->assertStatus(404)
+            ->assertSee('<title>Not Found</title>', false);
+
+        // A `JsonApi` call to non `api` route should return a default laravel json response.
+        $this->getJsonApi('non/api/route')
+            ->assertStatus(404)
+            ->assertJson(['message' => 'The route non/api/route could not be found.']);
     }
 }
